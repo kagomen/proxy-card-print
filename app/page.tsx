e2491a1cb7fb56077bebcb40bba77857b539ab9a
kagome <fu.taz.1102@gmail.com>
2024-11-26 16:39:57 +0900

    chore: shadcn/uiのセットアップ

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <p>Hello World!</p>
+      <Button size="sm" variant="ghost">
+        Hi!
+      </Button>
     </>
   );
 }
