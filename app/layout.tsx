ba2a33b02cc8e5784ae16c5224626b9540bcab60
kagome <fu.taz.1102@gmail.com>
2024-11-26 22:00:03 +0900

    chore: Next Fontのセットアップ #8

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,19 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter, M_PLUS_2 } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const mplus2 = M_PLUS_2({
+  preload: false,
+  variable: "--font-mplus2",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Proxy Card Print",
@@ -12,7 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html
+      lang="ja"
+      className={cn(inter.variable, mplus2.variable, "font-sans")}
+    >
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <p>Hello World!</p>
+      <p>Hello World! こんにちは</p>
       <Button size="sm" variant="ghost">
         Hi!
       </Button>
